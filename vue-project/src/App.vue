fbfa5732dc74aba99d9061ba80d3c0b154e1959c
Michal Nithesh <michalnithesh.cs22@stellamaryscoe.edu.in>
2024-07-02 00:14:59 +0530

    Vue.js Started

---
+++ vue-project/src/App.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { ref } from 'vue';
+
+      const name = ref('Michal');
+      const status = ref('active');
+      const tasks = ref(['Task One', 'Task Two', 'Task Three']);
+
+      const toggleStatus = () => {
+        if (status.value === 'active') {
+          status.value = 'pending';
+        } else if (status.value === 'pending'){
+          status.value = 'inactive'
+        } else {
+          status.value = 'active'
+        }
+      };
+</script>
+
+
+
+<template>
+  <h1>{{ name }}</h1>
+  <p v-if="status === 'active'">User is active</p>
+  <p v-else>User is inactive</p>
+
+  <h3>Tasks:</h3>
+  <ul>
+    <li v-for="task in tasks" :key="task">{{ task }}</li>
+  </ul>
+  <br />
+  <button @click="toggleStatus">Chage Status</button>
+</template>
+
